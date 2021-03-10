1126346572163ed4591149b267e6d11987d3d146
prentice.wongvibulsi <me@prenticew.com>
2021-03-10 14:43:59 -0800

    fix dict

--- managersite/manager/aws.py
+++ managersite/manager/aws.py
@@ -4,14 +4,20 @@ import boto3
 def _get_client():
   return boto3.client('ec2')
 
+def _get_or_empty(d, k):
+  if k in d:
+    return d[k]
+  else:
+    return ""
+
 def get_instance_info(iid):
   client = _get_client()
   info = client.describe_instances(InstanceIds=[iid])
   instance = info['Reservations'][0]['Instances'][0] # TODO: fail gracefully
   return {
       "state": instance['State']['Name'],
-      "ip": instance['PublicIpAddress'],
-      "launch_time": instance['LaunchTime'],
+      "ip": _get_or_empty(instance, 'PublicIpAddress'),
+      "launch_time": _get_or_empty(instance, 'LaunchTime'),
   }
 
 def stop_instance(iid):
