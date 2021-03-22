275ce8568829b1ea80a97cbb58f4daabc132ce85
Prentice Wongvibulsin <me@prenticew.com>
2021-03-21 23:23:34 -0700

    connect to server and shutdown gracefully (#10)

--- managersite/manager/models.py
+++ managersite/manager/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Ec2Instance(models.Model):
   instance_id = models.CharField(max_length=19)
   name = models.CharField(max_length=200)
+  ssh_key = models.TextField()
+  ssh_user = models.CharField(max_length=50)
+  ssh_url = models.CharField(max_length=200)
 
   def __str__(self):
     return self.name
