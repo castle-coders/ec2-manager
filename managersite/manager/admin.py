8cc0eef7f1bb1715bafe062d7cf41c607774179b
Prentice Wongvibulsin <me@prenticew.com>
2021-03-22 19:58:58 -0700

    automatically shutdown server after 15 min idle

--- managersite/manager/admin.py
+++ managersite/manager/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Ec2Instance
+from .models import Ec2Instance, ServerStatus
 
 
 admin.site.register(Ec2Instance)
+admin.site.register(ServerStatus)
--- managersite/manager/models.py
+++ managersite/manager/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 class Ec2Instance(models.Model):
@@ -6,6 +7,15 @@ class Ec2Instance(models.Model):
   ssh_key = models.TextField()
   ssh_user = models.CharField(max_length=50)
   ssh_url = models.CharField(max_length=200)
+  api_key = models.CharField(max_length=36, default=uuid.uuid4)
 
   def __str__(self):
     return self.name
+
+class ServerStatus(models.Model):
+  timestamp = models.DateTimeField(auto_now=True)
+  player_count = models.IntegerField()
+  for_server = models.ForeignKey(Ec2Instance, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return "{for_server} @ {timestamp} : {player_count}".format(timestamp=self.timestamp, player_count=self.player_count, for_server=self.for_server)
